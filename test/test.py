39c25cf262d145b47f6eb10d8486e1c7d59c5c38
FredrikKnudsen <fredrk@hiof.no>
2022-10-27 17:30:13 +0200

    Testing of GitHub actions #6

---
+++ test/test.py
@@ -0,0 +1,19 @@
+from softwareOblig.Main.function import isLeapYear
+
+
+def test_if_divisible_by_4_but_not_by_100_is_leap_year():
+    assert isLeapYear(1944)
+
+
+def test_if_divisible_by_400_is_leap_year():
+    assert isLeapYear(2000)
+
+
+def test_if_not_divisible_by_4_not_leap_year():
+    assert not isLeapYear(1993)
+
+
+def test_if_divisible_by_100_but_not_400_is_leap_year():
+    assert not isLeapYear(1900)
+
+#Change to upload
